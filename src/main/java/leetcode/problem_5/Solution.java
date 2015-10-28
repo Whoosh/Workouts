bba001cef9f47c9c3c5b348ba7568e363435dd39
Whoosh <whoosh.s@gmail.com>
2015-10-28 08:16:01 +0300

    leetcode_5 analyzed Manacher algorithm x50 faster than first one

--- src/main/java/leetcode/problem_5/Solution.java
+++ src/main/java/leetcode/problem_5/Solution.java
@@ -20,6 +20,12 @@ import java.util.concurrent.TimeUnit;
 public class Solution {
 
     public static void main(String[] args) {
+        runFirst();
+        System.out.println(longestPalindrome("jh1k23saippuakivikauppiasasdjknksjdfbjbdbbdbbdbd"));
+        System.out.println(longestPalindrome("add"));
+    }
+
+    private static void runFirst() {
         Options opt = new OptionsBuilder()
                 .include(Solution.class.getName())
                 .warmupIterations(10)
@@ -31,18 +37,65 @@ public class Solution {
         } catch (RunnerException e) {
             System.out.println("Benchmark broken, caught by " + e);
         }
-        System.out.println(longestPalindrome("jh1k23saippuakivikauppiasasdjknksjdfbjbdbbdbbdbd"));
-        System.out.println(longestPalindrome("add"));
-        System.out.println(longestPalindrome("1"));
-        System.out.println(longestPalindrome(""));
-        System.out.println(longestPalindrome("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"));
-        System.out.println(longestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+    }
+
+    @Benchmark
+    public static void run2() {
+        longestPalindrome2("jh1k23saippuakivikauppiasasdjknksjdfbjbdbbdbbdbd");
+        longestPalindrome2(getString());
     }
 
     @Benchmark
     public static void run() {
         longestPalindrome("jh1k23saippuakivikauppiasasdjknksjdfbjbdbbdbbdbd");
-        longestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        longestPalindrome(getString());
+    }
+
+    public static String getString(){
+        int size = 5_000;
+        StringBuilder builder = new StringBuilder(size);
+        for(int i=0; i<size; i++){
+            builder.append("a");
+            builder.insert(i%125,(char) (i%50)+35);
+        }
+        return builder.toString();
+    }
+
+    public static String longestPalindrome2(String src) {
+        int[] palindromeMap = new int[src.length() * 2 + 3];
+        char[] markedString = new char[palindromeMap.length];
+        char[] srcChars = src.toCharArray();
+
+        int center = 0, right = 0, mirror = 0;
+        int length = 0, pointOfPCenter = 0;
+
+        markedString[0] = '\t';
+        markedString[markedString.length - 1] = '\n';
+        markedString[palindromeMap.length - 2] = '\r';
+
+        for (int i = 0, i2 = 0; i < src.length(); i2 = 2 * ++i) {
+            markedString[i2 + 1] = '\r';
+            markedString[i2 + 2] = srcChars[i];
+        }
+
+        for (int i = 1; i < markedString.length - 1; i++, mirror = 2 * center - i) {
+            if (right > i)
+                palindromeMap[i] = Math.min(right - i, palindromeMap[mirror]);
+            while (markedString[i + (1 + palindromeMap[i])] == markedString[i - (1 + palindromeMap[i])])
+                palindromeMap[i]++;
+            if (i + palindromeMap[i] > right) {
+                center = i;
+                right = i + palindromeMap[i];
+            }
+        }
+
+        for (int i = 1; i < palindromeMap.length - 1; i++) {
+            if (palindromeMap[i] > length) {
+                length = palindromeMap[i];
+                pointOfPCenter = i;
+            }
+        }
+        return src.substring((pointOfPCenter - 1 - length) / 2, (pointOfPCenter - 1 + length) / 2);
     }
 
     public static String longestPalindrome(String s) {
