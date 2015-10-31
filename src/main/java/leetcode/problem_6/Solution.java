348d2821a649d5d043eeb6e8ff8f13fc6dae3199
Whoosh <whoosh.s@gmail.com>
2015-11-01 01:14:48 +0300

    leetcode_6 edit solution 4ms , funny to bit 99.80% of people.

--- src/main/java/leetcode/problem_6/Solution.java
+++ src/main/java/leetcode/problem_6/Solution.java
@@ -6,56 +6,40 @@ package leetcode.problem_6;
 public class Solution {
 
     public static void main(String[] args) {
-        int len = 2;
-        int[] map = new int[len];
-        for (int i = 0; i < len; i++) {
-            map[i] = i + 1;
-        }
-        System.out.println(convert("PAYPALISHIRING", 12));
+        System.out.println(convert("PAYPALISHIRING", 5));
     }
-
+    
     public static String convert(String src, int numRows) {
-        if (numRows < 2) return src;
-        if (src.length() == 1) return src;
-        if (src.length() <= numRows) return src;
-        char[] s = src.toCharArray();
-        StringBuilder result = new StringBuilder();
-        int len = s.length;
-        int index = 0;
+        int len = src.length();
+        if (numRows < 2 || len <= numRows) return src;
+
+        char[] source = src.toCharArray();
+        char[] result = new char[len];
+
+        int result_i;
+        int source_i;
+
         int topStep = (numRows - 1) * 2;
         int minStep = 0;
 
-        result.append(s[index]);
-        while ((index += topStep) < len) {
-            result.append(s[index]);
-        }
-        for (int i = 2; i < numRows; i++) {
-            index = 2 * numRows - i;
-            topStep -= 2;
+        result_i = fillFloat(result, 0, source, 0, len, topStep);
+
+        for (int i = 3; i <= numRows; i++) {
+            source_i = 2 * numRows - i;
+            result[result_i++] = source[i - 2];
             minStep += 2;
-            result.append(s[i - 1]);
-            for (int j = 2; index <= len; j++) {
-                result.append(s[index - 1]);
-                if ((j & 1) == 0) index += minStep;
-                else index += topStep;
+            for (int j = 2; source_i < len; j++) {
+                result[result_i++] = source[source_i];
+                source_i += (j & 1) == 0 ? minStep : topStep - minStep;
             }
         }
-        index = numRows;
-        result.append(s[index - 1]);
-        topStep = (numRows - 1) * 2;
-        while ((index += topStep) <= len) {
-            result.append(s[index - 1]);
-        }
-
-        return result.toString();
+        fillFloat(result, result_i, source, numRows - 1, len, (numRows - 1) * 2);
+        return new String(result);
     }
 
-    private static int second(int row, int prev) {
-        return 0;
+    private static int fillFloat(char[] result, int result_i, char[] source, int source_i, int len, int topStep) {
+        result[result_i++] = source[source_i];
+        while ((source_i += topStep) < len) result[result_i++] = source[source_i];
+        return result_i;
     }
-
-    private static int first(int row, int prev) {
-        return 0;
-    }
-
 }
