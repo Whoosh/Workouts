a8b64eab63c8aa00e0f6bf9db339c3d245bac99d
Whoosh <whoosh.s@gmail.com>
2015-11-10 21:39:37 +0300

    stepic algs module 2_1

---
+++ src/main/java/stepic/algs_base_1/module_2/K.java
@@ -0,0 +1,24 @@
+package stepic.algs_base_1.module_2;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * Created by whoosh on 11/10/15.
+ */
+public class K {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt(), i = 0, s = 0;
+        int z = k;
+        HashSet<Integer> set = new HashSet<>();
+        while (k > 0) {
+            k -= ++i;
+            s += i;
+            set.add(i);
+        }
+        set.remove(s - z);
+        System.out.println(set.size());
+        set.forEach(x -> System.out.print(x + " "));
+    }
+}
