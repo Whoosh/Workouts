ab977368d100589ba095c82b1aef3049822e0b8b
Whoosh <whoosh.s@gmail.com>
2015-12-18 08:58:19 +0300

    git got some bug within those files

---
+++ src/main/java/leetcode/problem_11/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.problem_11;
+
+/**
+ * Created by whoosh on 11/21/15.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] height = new int[]{1,1,1,2,3,4,4,4,1,2,5};
+        System.out.println(maxArea(height));
+    }
+    public static int maxArea(int[] height) {
+
+        return 1;
+    }
+}
---
+++ src/main/java/stepic/algs_csc_base_1/simply_tasks/FibGCD.java
@@ -0,0 +1,24 @@
+package stepic.algs_csc_base_1.simply_tasks;
+
+/**
+ * Created by whoosh on 11/4/15.
+ */
+
+public class FibGCD {
+
+    public static void main(String[] args) {
+        long a = 0, b = 1, c;
+        for (long i = 0; i < 70; i++) {
+            System.out.println(a + " " + b + " " + gcd(a, b));
+            c = b;
+            b = a + b;
+            a = c;
+        }
+    }
+
+    public static long gcd(long a, long b) {
+        if (a == 0) return b;
+        if (b == 0) return a;
+        return a < b ? gcd(b % a, a) : gcd(a % b, b);
+    }
+}
