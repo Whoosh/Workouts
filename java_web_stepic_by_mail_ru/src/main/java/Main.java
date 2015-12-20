3f69f55357b48fa60cdd109de775d1c323a4f90b
Whoosh <whoosh.s@gmail.com>
2015-12-20 17:17:33 +0300

    stepic web course SimplyServlet

---
+++ java_web_stepic_by_mail_ru/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.SimplyMirrorServlet;
+
+/**
+ * Created by whoosh on 12/18/15.
+ */
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        SimplyMirrorServlet sm = new SimplyMirrorServlet();
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(sm), "/mirror");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        System.out.println("Server started");
+        server.join();
+    }
+}
