e979cc242166d9516e0501671854bcb12f7bd48c
Whoosh <whoosh.s@gmail.com>
2015-12-22 19:17:34 +0300

    stepic mail course java web, few test updates.

--- java_web_stepic_by_mail_ru/src/main/java/tazk_2/Main.java
+++ java_web_stepic_by_mail_ru/src/main/java/tazk_2/Main.java
@@ -6,7 +6,6 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import tazk_2.profiles.UserProfile;
 import tazk_2.service.AccountService;
 import tazk_2.servlets.EchoServlet;
 import tazk_2.servlets.SessionsServlet;
@@ -15,20 +14,24 @@ import tazk_2.servlets.UsersServlet;
 /**
  * Created by whoosh on 12/21/15.
  */
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        startServer().join();
+    }
+
+    public static Server startServer() throws Exception {
         AccountService accountService = new AccountService();
 
-        accountService.addNewUser(new UserProfile("admin"));
-        accountService.addNewUser(new UserProfile("test"));
-
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
         context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
         context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/api/v1/sessions");
         context.addServlet(new ServletHolder(new EchoServlet()), "/echo");
 
         ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setResourceBase("public_html");
+
+        resource_handler.setResourceBase("src/main/resources/public_html");
 
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[]{resource_handler, context});
@@ -38,5 +41,6 @@ public class Main {
 
         server.start();
         System.out.println("Server started");
+        return server;
     }
 }
--- java_web_stepic_by_mail_ru/src/test/java/task_2/ServerRegistrationTest.java
+++ java_web_stepic_by_mail_ru/src/test/java/task_2/ServerRegistrationTest.java
@@ -1,5 +1,7 @@
 package task_2;
 
+import org.eclipse.jetty.server.Server;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,6 +9,7 @@ import tazk_2.Main;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
@@ -21,34 +24,64 @@ import java.util.concurrent.Future;
 public class ServerRegistrationTest {
 
     private static final String USER_AGENT = "Mozilla/5.0";
-    private static final String GET_URL = "http://localhost:8080/echo";
-    private static final String ECHO_URL = "http://localhost:8080/";
-    private static final String POST_URL = "http://localhost:8080/";
-    private static final String POST_PARAMS = "a=b";
-    private Thread testServerThread;
-    // TODO
+    private static final String ECHO_URL = "http://localhost:8080/echo";
+    private static final String REGISTRATION_URL = "http://localhost:8080/signup";
+    private static final String LOG_IN_URL = "http://localhost:8080/signin";
+    private static final String LOGIN_PARAM = "login=";
+    private static final String PASSWORD_PARAM = "password=";
+    private static final String LOGIN = "man";
+    private static final String PASSWORD = "mans password";
+
+    private Server server;
+
     @Before
     public void runServer() throws Exception {
         ExecutorService executorService = Executors.newCachedThreadPool();
-        Future<?> submit = executorService.submit(() -> {
+        Future<Server> submit = executorService.submit(() -> {
             try {
-                Main.main(null);
+                return Main.startServer();
             } catch (Exception e) {
                 e.printStackTrace();
+                return null;
             }
         });
-        submit.get();
+        server = submit.get();
+    }
+
+    @After
+    public void stopServer() throws Exception {
+        server.stop();
     }
 
     @Test
     public void isServerAliveTest() throws IOException {
         String message = "hello";
-        URL obj = new URL(GET_URL + "?param=" + message);
+        URL obj = new URL(ECHO_URL + "?param=" + message);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("User-Agent", USER_AGENT);
         Assert.assertEquals(HttpServletResponse.SC_OK, con.getResponseCode());
         Scanner scanner = new Scanner(con.getInputStream());
-        Assert.assertEquals("hello", scanner.nextLine());
+        Assert.assertEquals(message, scanner.nextLine());
+    }
+
+    @Test
+    public void tryToRegisterTest() throws Exception {
+        URL obj = new URL(REGISTRATION_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setDoOutput(true);
+        OutputStream os = con.getOutputStream();
+        os.write(LOGIN_PARAM.getBytes());
+        os.write(LOGIN.getBytes());
+        os.write(',');
+        os.write(PASSWORD_PARAM.getBytes());
+        os.write(PASSWORD.getBytes());
+        os.flush();
+        os.close();
+        Assert.assertEquals(HttpServletResponse.SC_OK, con.getResponseCode());
+        Scanner scanner = new Scanner(con.getInputStream());
+        System.out.println(scanner.nextLine());
     }
 }
