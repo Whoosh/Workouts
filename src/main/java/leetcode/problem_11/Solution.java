9c3ab7859ec09bd051bae91f2bc76373231dc600
Whoosh <whoosh.s@gmail.com>
2016-02-07 18:17:41 +0300

    LeetCode 11

--- src/main/java/leetcode/problem_11/Solution.java
+++ src/main/java/leetcode/problem_11/Solution.java
@@ -1,15 +1,20 @@
 package leetcode.problem_11;
 
-/**
- * Created by whoosh on 11/21/15.
- */
 public class Solution {
     public static void main(String[] args) {
-        int[] height = new int[]{1,1,1,2,3,4,4,4,1,2,5};
+        int[] height = new int[]{1, 1, 1, 2, 3, 4, 4, 4, 1, 2, 5};
         System.out.println(maxArea(height));
     }
-    public static int maxArea(int[] height) {
 
-        return 1;
+    public static int maxArea(int[] h) {
+        int maxFounded = 0;
+        int start = 0;
+        int end = h.length - 1;
+        int b, n;
+        while ((n = end - start) > 0) {
+            b = h[h[start] < h[end] ? start++ : end--] * n;
+            if (b > maxFounded) maxFounded = b;
+        }
+        return maxFounded;
     }
 }
---
+++ src/test/java/problem_11/TestSolution.java
@@ -0,0 +1,26 @@
+package problem_11;
+
+import leetcode.problem_11.Solution;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by whoosh on 2/7/16.
+ */
+
+public class TestSolution {
+
+    @Test
+    public void maxAreaTest() {
+        int first = Solution.maxArea(new int[]{1, 1});
+        Assert.assertEquals(1, first);
+        int second = Solution.maxArea(new int[]{1,1,3,5,1,2,6,1,10,2,18,1,29,1,23,4,1,2,5,7,8,5,3,1,3,67,8,8,1});
+        Assert.assertEquals(377, second);
+        int third = Solution.maxArea(new int[]{0});
+        Assert.assertEquals(0, third);
+        int fourth = Solution.maxArea(new int[]{});
+        Assert.assertEquals(0, fourth);
+        int fifth = Solution.maxArea(new int[]{3,4,5,1,2,3,5,2,5});
+        Assert.assertEquals(30, fifth);
+    }
+}
