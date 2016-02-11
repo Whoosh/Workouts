8c8419531e4225cb29689dfbad78d3e77a7cda7f
Whoosh <whoosh.s@gmail.com>
2016-02-11 11:38:20 +0300

    LeetCode 16

---
+++ src/main/java/leetcode/problem_16/Solution.java
@@ -0,0 +1,32 @@
+package leetcode.problem_16;
+
+import java.util.Arrays;
+
+/**
+ * Created by whoosh on 2/11/16.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(threeSumClosest(new int[]{-123, 0, 1, 1, 1, 2, 22, 2, 2, 2, 2, 2, 2, -1, 1, 1, -2312, 123, -40, -122}, 50));
+    }
+
+    public static int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int sum = Integer.MAX_VALUE;
+        int d = Integer.MAX_VALUE;
+        int t;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1, k = nums.length - 1; k > j; ) {
+                final int bSum = nums[i] + nums[j] + nums[k];
+                if (bSum == target) return target;
+                if (bSum > target) k--;
+                else j++;
+                if ((t = Math.abs(bSum - target)) < d) {
+                    d = t;
+                    sum = bSum;
+                }
+            }
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/problem_15/TestSolution.java
@@ -0,0 +1,28 @@
+package problem_15;
+
+import leetcode.problem_15.Solution;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by whoosh on 2/11/16.
+ */
+public class TestSolution {
+
+    @Test
+    public void randomSizeAndDataArrayTest(){
+        int seed = 100;
+        int[] data = new int[new Random().nextInt(seed)];
+        Random random = new Random(seed);
+        for(int i=0; i<data.length; i++){
+            data[i] = random.nextInt(10);
+            if((i&1) == 0) data[i] = -data[i];
+        }
+        List<List<Integer>> lists = Solution.threeSum(data);
+        for (List<Integer> list : lists) {
+            System.out.println(list);
+        }
+    }
+}
