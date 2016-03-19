e96e2415197128a4976f00374499ee162bcc42ed
Whoosh <whoosh.s@gmail.com>
2016-03-19 23:20:35 +0300

    just test commit settings with token

---
+++ src/main/java/leetcode/problems_300_400/problem_336/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.problems_300_400.problem_336;
+
+/**
+ * Created by whoosh on 2/29/16.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        // just test smt, coming soon..
+    }
+}
